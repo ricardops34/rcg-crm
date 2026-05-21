72945fc7bf4edf0be6159d5e21ef1297cc92794f
ricardops34 <ricardopataysotomayor@gmail.com>
2026-05-20 20:42:37 -0400

    fir

---
+++ frontend/src/app/services/tabela-preco.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from "../../environments/environment";
+
+@Injectable({
+  providedIn: "root"
+})
+export class TabelaPrecoService {
+
+  private readonly API_URL = `${environment.apiUrl}/commercial/tabelas-precos`;
+
+  constructor(private http: HttpClient) { }
+
+  private getHeaders() {
+    const token = localStorage.getItem("token");
+    return new HttpHeaders().set("Authorization", `Bearer ${token}`);
+  }
+
+  findAll(): Observable<any> {
+    return this.http.get<any>(this.API_URL, { headers: this.getHeaders() });
+  }
+
+  findOne(id: number): Observable<any> {
+    return this.http.get<any>(`${this.API_URL}/${id}`, { headers: this.getHeaders() });
+  }
+}
